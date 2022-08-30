207531061da64ffaa64450eff1aa21eca0955d7f
Bilal Shuja <bilalshuja77@gmail.com>
2022-08-30 19:09:44 +0500

    gym-dashboard

---
+++ src/Components/ManageContent/WebsiteSections/HowToManageWebDashTuts/ManageWebsiteTuts.jsx
@@ -0,0 +1,166 @@
+import React from 'react';
+
+
+const ManageWebsiteTuts = () => {
+  return (
+    <>
+    <div className="app-content content ">
+  <div className="content-wrapper container-xxl p-0">
+    <div className="content-header row">
+
+    <div className="content-header-left col-md-9 col-12 mb-2">
+        <div className="row breadcrumbs-top">
+          <div className="col-12">
+            <h2 className="content-header-title float-start mb-0">How to Manage Website</h2>
+            <div className="breadcrumb-wrapper">
+              <ol className="breadcrumb">
+                <li className="breadcrumb-item"><a href="/ManageWebDashScreens">Home</a>
+                </li>
+              </ol>
+            </div>
+          </div>
+        </div>
+      </div>
+</div>
+<div className="content-body">
+{/* Accordion with margin start */}
+<section id="accordion-with-margin">
+  <div className="row">
+    <div className="col-sm-12">
+      <div className="card">
+        <div className="card-body">
+          {/* <p className="card-text">
+            To create accordion with margin use <code>.accordion-margin</code> class as a wrapper for your accordion
+            header.
+          </p> */}
+          <div className="accordion accordion-margin" id="accordionMargin">
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="headingMarginOne">
+                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#accordionMarginOne" aria-expanded="false" aria-controls="accordionMarginOne">
+                How update your website Header Section
+                </button>
+              </h2>
+              <div id="accordionMarginOne" className="accordion-collapse collapse" aria-labelledby="headingMarginOne" data-bs-parent="#accordionMargin">
+                <div className="accordion-body">
+                
+                <section id="media-player-wrapper">
+                <div class="row">
+                  <div className="col-12">
+                    <div className="card">
+                      <div className="card-body">
+                          <iframe className="embed-responsive-item w-100" src="https://www.youtube.com/embed/AroK04i2MYk" allowFullScreen style={{minHeight:"32em"}}/>
+                      </div>
+                    </div>
+                  </div>
+                  </div>
+                  </section>
+                  </div>
+              </div>
+            </div>
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="headingMarginTwo">
+                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#accordionMarginTwo" aria-expanded="false" aria-controls="accordionMarginTwo">
+                How to update your website choose your program section
+                </button>
+              </h2>
+              <div id="accordionMarginTwo" className="accordion-collapse collapse" aria-labelledby="headingMarginTwo" data-bs-parent="#accordionMargin">
+                <div className="accordion-body">
+                <section id="media-player-wrapper">
+                <div class="row">
+                  <div className="col-12">
+                    <div className="card">
+                      <div className="card-body">
+                          <iframe className="embed-responsive-item w-100" src="https://www.youtube.com/embed/yv_lLmhhILY" allowFullScreen style={{minHeight:"32em"}}/>
+                      </div>
+                    </div>
+                  </div>
+                  </div>
+                  </section>
+                </div>
+              </div>
+            </div>
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="headingMarginThree">
+                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#accordionMarginThree" aria-expanded="false" aria-controls="accordionMarginThree">
+                  How to update your website pricing section
+                </button>
+              </h2>
+              <div id="accordionMarginThree" className="accordion-collapse collapse" aria-labelledby="headingMarginThree" data-bs-parent="#accordionMargin">
+                <div className="accordion-body">
+                <section id="media-player-wrapper">
+                <div class="row">
+                  <div className="col-12">
+                    <div className="card">
+                      <div className="card-body">
+                          <iframe className="embed-responsive-item w-100" src="https://www.youtube.com/embed/7Uja-_AHNoE" allowFullScreen style={{minHeight:"32em"}}/>
+                      </div>
+                    </div>
+                  </div>
+                  </div>
+                  </section>
+                </div>
+              </div>
+            </div>
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="headingMarginFour">
+                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#accordionMarginFour" aria-expanded="false" aria-controls="accordionMarginFour">
+                How to update your website about-us section
+                </button>
+              </h2>
+              <div id="accordionMarginFour" className="accordion-collapse collapse" aria-labelledby="headingMarginFour" data-bs-parent="#accordionMargin">
+                <div className="accordion-body">
+                <section id="media-player-wrapper">
+                <div class="row">
+                  <div className="col-12">
+                    <div className="card">
+                      <div className="card-body">
+                          <iframe className="embed-responsive-item w-100" src="https://www.youtube.com/embed/8MKRyu1Hy6E" allowFullScreen style={{minHeight:"32em"}}/>
+                      </div>
+                    </div>
+                  </div>
+                  </div>
+                  </section>
+                </div>
+              </div>
+            </div>
+
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="headingMarginFive">
+                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#accordionMarginFive" aria-expanded="false" aria-controls="accordionMarginFive">
+                How to update your website gallery &amp; social media links section
+                </button>
+              </h2>
+              <div id="accordionMarginFive" className="accordion-collapse collapse" aria-labelledby="headingMarginFive" data-bs-parent="#accordionMargin">
+                <div className="accordion-body">
+                <section id="media-player-wrapper">
+                <div class="row">
+                  <div className="col-12">
+                    <div className="card">
+                      <div className="card-body">
+                          <iframe className="embed-responsive-item w-100" src="https://www.youtube.com/embed/DDoMDwzLehs" allowFullScreen style={{minHeight:"32em"}}/>
+                      </div>
+                    </div>
+                  </div>
+                  </div>
+                  </section>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+{/* Accordion with margin end */}
+    
+    
+    </div>
+</div>
+</div>
+
+    </>
+  )
+}
+
+export default ManageWebsiteTuts
